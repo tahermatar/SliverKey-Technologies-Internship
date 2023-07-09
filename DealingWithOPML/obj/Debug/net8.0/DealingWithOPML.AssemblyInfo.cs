576ed2060d273afbc513bf12c356dc2f0af66b31
taher <hp@DESKTOP-DQC5HB0>
2023-07-09 10:41:55 +0300

    upload-gitignore

--- DealingWithOPML/obj/Debug/net8.0/DealingWithOPML.AssemblyInfo.cs
+++ DealingWithOPML/obj/Debug/net8.0/DealingWithOPML.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DealingWithOPML")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa78ce0d41bc3db9ab219850a5e13074f40406cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("DealingWithOPML")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DealingWithOPML")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
