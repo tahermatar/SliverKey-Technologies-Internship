bb0e6c80e680b1a23591cd25480d7477aadd8f62
taher <hp@DESKTOP-DQC5HB0>
2023-07-15 16:13:05 +0300

    upload project

---
+++ CurrentTimeService/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.MapGet("api/datetime", () =>
+{
+    return DateTime.Now.ToString();
+});
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.Run();
