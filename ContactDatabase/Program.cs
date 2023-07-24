044f7f5db1666e6d4c02a738ecacc67815733033
taher <hp@DESKTOP-DQC5HB0>
2023-07-24 17:29:11 +0300

    update based on assignment 11

--- ContactDatabase/Program.cs
+++ ContactDatabase/Program.cs
@@ -1,5 +1,9 @@
 using EdgeDB;
 using EdgeDB.State;
+using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
 using static ContactDatabase.Pages.IndexModel;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +11,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddSession();
 builder.Services.AddRazorPages();
+builder.Services.AddAntiforgery();
+builder.Services.AddHttpClient();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
 builder.Services.AddEdgeDB(EdgeDBConnection.FromInstanceName("Edgedb_Example"), config =>
 {
     config.SchemaNamingStrategy = INamingStrategy.SnakeCaseNamingStrategy;
@@ -28,8 +35,60 @@ app.UseStaticFiles();
 app.UseSession();
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
+
+app.MapPost("/login", async (HttpContext httpContext, IAntiforgery antiforgery) =>
+{
+    await antiforgery.ValidateRequestAsync(httpContext);
+
+    string? username = httpContext.Request.Form["username"];
+    string? password = httpContext.Request.Form["password"];
+
+    if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+    {
+        httpContext.Response.Redirect("/?errorMessage=Username%20and%20password%20are%20required.");
+        return;
+    }
+
+    if ((username == "taher" && password == "taher") || (username == "youssef" && password == "youssef"))
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, username),
+            new Claim(ClaimTypes.Role, "Normal")
+        };
+
+        if (username == "taher")
+            claims[1] = new Claim(ClaimTypes.Role, "Admin");
+
+        var claimsIdentity = new ClaimsIdentity(
+            claims,
+            CookieAuthenticationDefaults.AuthenticationScheme
+        );
+
+        await httpContext.SignInAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme,
+            new ClaimsPrincipal(claimsIdentity)
+        );
+
+        httpContext.Response.Redirect("/");
+    }
+    else
+    {
+        httpContext.Response.Redirect("/?errorMessage=Invalid%20username%20or%20password.");
+    }
+});
+
+app.MapPost("/logout", async (HttpContext httpContext) =>
+{
+    await httpContext.SignOutAsync(
+        CookieAuthenticationDefaults.AuthenticationScheme
+    );
+
+    httpContext.Response.Redirect("/");
+});
 
 app.Run();
